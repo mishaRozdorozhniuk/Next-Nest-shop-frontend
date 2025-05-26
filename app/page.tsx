a97e9d422a2f3dacc66bb7e241da3de622c9a0f0
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-05-26 19:38:31 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import CreateProductFab from './products/create-product/create-product-fab';
-import Products from './products/products';
+// import CreateProductFab from './products/create-product/create-product-fab';
+// import Products from './products/products';
 
 export default async function Home() {
   return (
     <>
-      <Products />
-      <CreateProductFab />
+      hi
+      {/* <Products />
+      <CreateProductFab /> */}
     </>
   );
 }
