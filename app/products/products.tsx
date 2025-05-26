73fb20cd27ada29e99ec2f9144ac03c14b7b0753
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-05-26 19:43:31 +0200

    test

--- app/products/products.tsx
+++ app/products/products.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import getProducts from './actions/get-product';
-import ProductsGrid from './products-grid';
+// import ProductsGrid from './products-grid';
 
 export default async function Products() {
   const products = await getProducts();
 
   console.log('Products:', products);
 
-  return <ProductsGrid products={products} />;
+  // return <ProductsGrid products={products} />;
 }
