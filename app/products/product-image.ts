762122d4309c71d06de444175022f5442a8a49bd
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-05-26 15:29:03 +0200

    fix images

--- app/products/product-image.ts
+++ app/products/product-image.ts
@@ -1,5 +1,3 @@
-import { API_URL } from '../common/constants/api';
-
 export const getProductImage = (productId: number, ext: string) => {
-  return `${API_URL}/images/products/${productId}${ext}`;
+  return `${process.env.NEXT_PUBLIC_API_URL}/images/products/${productId}${ext}`;
 };
