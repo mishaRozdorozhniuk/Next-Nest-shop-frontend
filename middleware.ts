4ea3c4659689ae457fdf6093837189f677e11f4f
Mykhailo Rozdorozhniuk <misha.roz.2003@gmail.com>
2025-05-26 19:03:01 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { NextRequest } from 'next/server';
 import authenticated from './app/auth/actions/authenticated';
-import { onAuthenticated } from './app/common/constants/routes';
+// import { onAuthenticated } from './app/common/constants/routes';
 
 export async function middleware(req: NextRequest) {
   const isAuth = await authenticated();
-
-  if (!isAuth && !onAuthenticated.some(route => req.nextUrl.pathname.startsWith(route.path))) {
+  console.log(isAuth);
+  // && !onAuthenticated.some(route => req.nextUrl.pathname.startsWith(route.path)
+  if (!isAuth) {
     return Response.redirect(new URL('/auth/login', req.url));
   }
 }
